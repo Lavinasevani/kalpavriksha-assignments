70804c16750d25e3149c0175fb2db1ac5b11d82f
Lavinasevani <lavinasevani2432004@gmail.com>
2025-09-24 23:03:07 +0530

    Validity check of input string

--- Assigment1/calculator.c
+++ Assigment1/calculator.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <string.h> 
+#include<ctype.h>
 void cal(char inp[]){
     //White space handling
     int j =0 ;
@@ -10,6 +12,24 @@ void cal(char inp[]){
     }
     inp[j] = '\0';
     printf("White Space Ignored : %s",inp);
+
+    //Validity check 
+    size_t len = strlen(inp);
+    if(len > 0 && inp[len-1] == '\n') {
+        inp[len-1] = '\0';
+    }
+    int valid = 1;
+    for(int k = 0 ; inp[k] != '\0' ; k++){
+        if(inp[k] != '+' && inp[k] != '-' && inp[k] != '*' && inp[k] != '/' && !isdigit(inp[k])){
+            valid =0;
+            break;
+        }
+    }
+    if(valid == 0){
+        printf("Invalid expression.\n");
+        return;
+    }
+    printf("Expression evalution: ");
 }
 int main(){
     char exp[50];
