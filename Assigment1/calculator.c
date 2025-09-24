963266eb69e87b6660e6754fba88c7ecff1cba37
Lavinasevani <lavinasevani2432004@gmail.com>
2025-09-24 21:14:13 +0530

    White Space Ignored

--- Assigment1/calculator.c
+++ Assigment1/calculator.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
+void cal(char inp[]){
+    //White space handling
+    int j =0 ;
+    for(int i = 0 ; inp[i] != '\0' ; i++){
+        if(inp[i] != ' '){
+            inp[j] = inp[i];
+            j++;
+        }
+    }
+    inp[j] = '\0';
+    printf("White Space Ignored : %s",inp);
+}
 int main(){
     char exp[50];
     fgets(exp , sizeof(exp) , stdin); //String Input
     printf("Entered : %s",exp); 
-
+    cal(exp);
 }
