c7331c796ec486877f71ea24b7a94bc24938158e
Lavinasevani <lavinasevani2432004@gmail.com>
2025-09-25 13:39:41 +0530

    Build first number of exp

--- Assigment1/calculator.c
+++ Assigment1/calculator.c
@@ -29,7 +29,19 @@ void cal(char inp[]){
         printf("Invalid expression.\n");
         return;
     }
+    //Expression Evalution
     printf("Expression evalution: ");
+    int num = 0;
+    for(int i =0; inp[i] != '\0' ;i++){
+        char ch = inp[i];
+        if(isdigit(ch)){
+            num = num  * 10 +(ch-'0'); //Number 
+        }else{
+            break;
+        }
+    }
+    printf("%d",num);
+    
 }
 int main(){
     char exp[50];
