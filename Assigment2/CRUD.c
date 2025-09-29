f19a53af683715a3ce35c2046c50aecccf1a9b6d
Lavinasevani <lavinasevani2432004@gmail.com>
2025-09-29 22:35:16 +0530

    Create User Function Implemented

--- Assigment2/CRUD.c
+++ Assigment2/CRUD.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
+struct User {
+    int uid;
+    char name[100];
+    int age;
+};
+void createUser(){
+    FILE *fptr;
+    fptr = fopen("user.txt" , "a");
+    struct User u;
+    printf("Enter ID , Name , age (Saprated by space) :\n");
+    scanf("%d %s %d" , &u.uid , u.name ,&u.age); 
+    fprintf(fptr,"%d %s %d\n" ,u.uid,u.name,u.age);
+    fclose(fptr);
+    printf("User Created.\n");
+}
 int main(){
     int choice ;
-    int i =5;
     while(true)
     {
         printf("\nEnter the number to select your choice :\n1.Create User \n2.Read Users \n3.Update User \n4.Delete User \n5.Exit\n");
         scanf("%d",&choice);
         if(choice==1){
-            printf("User Created.");
+            createUser();
         }
         else if(choice==2){
             printf("All User fetched.");
