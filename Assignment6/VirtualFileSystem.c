7efce407539fbb43a59796e7398a53aa3740712c
Lavinasevani <lavinasevani2432004@gmail.com>
2025-11-07 00:18:49 +0530

    All directory commands implemented

--- Assignment6/VirtualFileSystem.c
+++ Assignment6/VirtualFileSystem.c
@@ -9,13 +9,13 @@
 
 struct FileNode{
     char name[MAX_STRING_LENGTH];
-    bool isDirectory;
-    struct FileNode *parent;
-    struct FileNode *child;
-    struct FileNode *next;
+    bool isDirectory; //directory or file 
+    struct FileNode *parent; // parent 
+    struct FileNode *child; // child 
+    struct FileNode *next; // sibling
 };
 
-char virtualDisk[BLOCK_SIZE][NUM_OF_BLOCKS];
+char virtualDisk[NUM_OF_BLOCKS][BLOCK_SIZE];
 
 struct FreeBlockNode{
     int index;
@@ -36,7 +36,7 @@ void initializeRootDirectory(){
     }
     strcpy(root->name, "/");
     root->isDirectory = true;
-    root->next = root;
+    root->next = NULL;
     root->parent =NULL;
     root->child = NULL;
     current = root;
@@ -55,17 +55,15 @@ void initializeFreeBlocks(){
         newBlock->prev = NULL;
         if(blockListHead == NULL){
             blockListHead = newBlock;
-            blockListTail = newBlock;
         }else{
             blockListTail->next = newBlock;
-            newBlock->prev = blockListTail;
-            blockListTail = newBlock;
         }
+        blockListTail = newBlock;
     }
     return;
 }
 
-void currentDirectory(){
+void printPrompt(){
     printf("\n%s > ", current->name);
     return;
 }
@@ -80,6 +78,175 @@ int getCommandIndex(const char *cmd) {
     }
     return -1;
 }
+void mkdirPerform(char *directoryName){
+    
+    struct FileNode *temp = current->child;
+    while (temp != NULL) {
+        if (strcmp(temp->name, directoryName) == 0 && temp->isDirectory) {
+            printf("Error: Directory '%s' already exists.\n", directoryName);
+            return;
+        }
+        temp = temp->next;
+    }
+
+    // remaining name validations :  
+    //A file name can't contain any of the following characters: \ / * ? < > : " |
+    //len can't be 0 means can't be empty , NULL , or caintain soace only 
+
+    struct FileNode *newDirectory = (struct FileNode*)malloc(sizeof(struct FileNode));
+    if(newDirectory == NULL){
+        printf("Memory alloaction fail");
+        exit(1);
+    }
+
+    strcpy(newDirectory->name, directoryName);
+    newDirectory->isDirectory = true;
+    newDirectory->next = root;
+    newDirectory->parent = current;
+    newDirectory->child = NULL; 
+    newDirectory->next = NULL; 
+
+    if (current->child == NULL) { // means there is no child of this current directory
+        current->child = newDirectory;
+    } else {
+        struct FileNode *temp2 = current->child;
+        while (temp2->next != NULL){ 
+            temp2 = temp2->next;
+        }
+        temp2->next = newDirectory;
+    }
+    printf("Directory '%s' created successfully.",directoryName);
+    return;
+}
+
+void rmdirPerform(char *dirName) {
+    if (dirName == NULL) {
+        printf("Error: Please provide a directory name.\n");
+        return;
+    }
+    if(strcmp(dirName, root->name) == 0){
+        printf("You can delete the root directory");
+        return;
+    }
+
+    struct FileNode *temp = current->child;
+    struct FileNode *prev = NULL;
+
+    while (temp != NULL) {
+        if (temp->isDirectory && strcmp(temp->name, dirName) == 0) {
+            if (temp->child != NULL) {
+                printf("Error: Directory '%s' is not empty.\n", dirName);
+                return;
+            }
+
+            if (prev == NULL) {
+                current->child = temp->next;
+            } else {
+                prev->next = temp->next;
+            }
+
+            free(temp);
+            printf("Directory '%s' removed successfully.\n", dirName);
+            return;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+
+    printf("Error: Directory '%s' not found.\n", dirName);
+}
+
+void lsPerform() {
+    if (current->child == NULL) {
+        printf("(empty)\n");
+        return;
+    }
+    struct FileNode *temp = current->child;
+    while (temp != NULL) {
+        printf("%s%s  \n", temp->name, temp->isDirectory ? "/" : "");
+        temp = temp->next;
+    }
+}
+
+void cwdPerform() {
+    if (current == NULL) {
+        printf("Error: No current directory.\n");
+        return;
+    }
+    printf("Current Directory: %s\n", current->name);
+}
+
+void pwdPerform() {
+    if (current == NULL) {
+        printf("Error: No current directory.\n");
+        return;
+    }
+    char paths[50][MAX_STRING_LENGTH];
+    int count = 0;
+    struct FileNode *temp = current;
+    while (temp != NULL) {
+        strcpy(paths[count], temp->name);
+        count++;
+        temp = temp->parent;
+    }
+    for (int i = count - 1; i >= 0; i--) {
+        if (i == count - 1 && strcmp(paths[i], "/") == 0)
+            printf("/");
+        else
+            printf("%s%s", (strcmp(paths[i], "/") == 0) ? "" : "/", paths[i]);
+    }
+    printf("\n");
+}
+
+
+void dfPerform() {
+    int totalBlocks = NUM_OF_BLOCKS;
+    int usedBlocks = 0;
+    struct FreeBlockNode *temp = blockListHead;
+
+    int freeBlocks = 0;
+    while (temp != NULL) {
+        freeBlocks++;
+        temp = temp->next;
+    }
+
+    usedBlocks = totalBlocks - freeBlocks;
+
+    double usagePercent = ((double)usedBlocks / totalBlocks) * 100.0;
+
+    printf("Total Blocks: %d\n", totalBlocks);
+    printf("Used Blocks: %d\n", usedBlocks);
+    printf("Free Blocks: %d\n", freeBlocks);
+    printf("Disk Usage: %.2f%%\n", usagePercent);
+}
+
+
+void cdPerform(char *dirName) {
+    if (dirName == NULL) {
+        printf("Error: Please provide a directory name.\n");
+        return;
+    }
+    
+    if (strcmp(dirName, "..") == 0) {
+        if (current->parent != NULL)
+            current = current->parent;
+        else
+            printf("Already at root.\n");
+        return;
+    }
+
+    struct FileNode *temp = current->child;
+    while (temp != NULL) {
+        if (temp->isDirectory && strcmp(temp->name, dirName) == 0) {
+            current = temp;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    printf("Error: Directory '%s' not found.\n", dirName);
+}
 
 void execute(char *input){
     if(input == NULL) return;
@@ -91,23 +258,27 @@ void execute(char *input){
     }
     switch (cmdIndex)
     {
-        case 0: 
-            printf("mkdir"); // mkdirPerform(); 
+        case 0: {
+            char *directoryName= strtok(NULL, " "); // NULL because same old one string the alredy been processed
+            mkdirPerform(directoryName); 
             break;
-        case 1: 
-            printf("rmdir"); // rmdirPerform(); 
+        }
+        case 1:{
+            char *directoryNameToDelete= strtok(NULL, " "); // NULL because same old one string the alredy been processed
+            rmdirPerform(directoryNameToDelete); 
             break;
+        }
         case 2: 
-            printf("pwd"); // pwdPerform(); 
+            pwdPerform(); 
             break;
         case 3:
-            printf("cwd"); //cwdPerform(); 
+            cwdPerform(); 
             break;
         case 4: 
-            printf("df"); //dfPerform(); 
+            dfPerform(); 
             break;
         case 5: 
-            printf("ls");    //lsPerform(); 
+            lsPerform(); 
             break;
         case 6: 
             printf("create");   //createPerform(); 
@@ -121,9 +292,11 @@ void execute(char *input){
         case 9: 
             printf("delete");   //deletePerform(); 
             break;
-        case 10: 
-            printf("cd");   //cdPerform(); 
+        case 10:{
+            char *argument= strtok(NULL, " "); 
+            cdPerform(argument); 
             break;
+        }
         default: 
             printf("Command not implemented yet.\n");
     }
@@ -140,7 +313,7 @@ int main() {
         return 1;
     }
     while (1) {
-        currentDirectory();
+        printPrompt();
         if (fgets(input, MAX_STRING_LENGTH, stdin) == NULL) break;
         input[strcspn(input, "\n")] = '\0';
         if (strlen(input) == 0)
