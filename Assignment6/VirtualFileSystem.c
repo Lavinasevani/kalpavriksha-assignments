0ddd71b2f2b83936629e7359a98b2282f278102c
Lavinasevani <lavinasevani2432004@gmail.com>
2025-11-06 20:22:12 +0530

    added command execution skeleton for VFS shell

--- Assignment6/VirtualFileSystem.c
+++ Assignment6/VirtualFileSystem.c
@@ -25,10 +25,10 @@ struct FreeBlockNode{
 
 struct FileNode *root = NULL;
 struct FileNode *current = NULL;
-struct FreeBlockNode *blockHead = NULL;
-struct FreeBlockNode *blockTail = NULL;
+struct FreeBlockNode *blockListHead = NULL;
+struct FreeBlockNode *blockListTail = NULL;
 
-void initalizeRootDirectory(){
+void initializeRootDirectory(){
     root = (struct FileNode*)malloc(sizeof(struct FileNode));
     if(root == NULL){
         printf("Memory allocation failed.");
@@ -43,29 +43,120 @@ void initalizeRootDirectory(){
     return;
 }
 
+void initializeFreeBlocks(){
+    for(int i =0 ; i < NUM_OF_BLOCKS ; i++){
+        struct FreeBlockNode *newBlock = (struct FreeBlockNode *)malloc(sizeof(struct FreeBlockNode));
+        if(newBlock == NULL){
+            printf("Memory allocation Failed");
+            exit(1);
+        }
+        newBlock->index = i;
+        newBlock->next = NULL;
+        newBlock->prev = NULL;
+        if(blockListHead == NULL){
+            blockListHead = newBlock;
+            blockListTail = newBlock;
+        }else{
+            blockListTail->next = newBlock;
+            newBlock->prev = blockListTail;
+            blockListTail = newBlock;
+        }
+    }
+    return;
+}
+
 void currentDirectory(){
     printf("\n%s > ", current->name);
     return;
 }
 
+const char *validCommands[] =  {"mkdir", "rmdir", "pwd", "cwd", "df", "ls", "create", "read", "write", "delete", "cd"};
+
+int getCommandIndex(const char *cmd) {
+    int numberOfCommands = sizeof(validCommands) / sizeof(validCommands[0]);
+    for (int i = 0; i < numberOfCommands; i++) {
+        if (strcmp(cmd, validCommands[i]) == 0)
+            return i;
+    }
+    return -1;
+}
+
+void execute(char *input){
+    if(input == NULL) return;
+    char *inputCommand = strtok(input," ");
+    int cmdIndex = getCommandIndex(inputCommand);
+    if (cmdIndex == -1) {
+        printf("Invalid Command");
+        return;
+    }
+    switch (cmdIndex)
+    {
+        case 0: 
+            printf("mkdir"); // mkdirPerform(); 
+            break;
+        case 1: 
+            printf("rmdir"); // rmdirPerform(); 
+            break;
+        case 2: 
+            printf("pwd"); // pwdPerform(); 
+            break;
+        case 3:
+            printf("cwd"); //cwdPerform(); 
+            break;
+        case 4: 
+            printf("df"); //dfPerform(); 
+            break;
+        case 5: 
+            printf("ls");    //lsPerform(); 
+            break;
+        case 6: 
+            printf("create");   //createPerform(); 
+            break;
+        case 7: 
+            printf("read"); //readPerform(); 
+            break;
+        case 8: 
+            printf("write");     //writePerform(); 
+            break;
+        case 9: 
+            printf("delete");   //deletePerform(); 
+            break;
+        case 10: 
+            printf("cd");   //cdPerform(); 
+            break;
+        default: 
+            printf("Command not implemented yet.\n");
+    }
+}    
+
 int main() {
     printf("$ ./vfs\nCompact VFS - ready. Type 'exit' to quit.");
 
-    initalizeRootDirectory();
-
-    char *choice = malloc(MAX_STRING_LENGTH * sizeof(char));
+    initializeRootDirectory();
+    initializeFreeBlocks();
+    char *input= malloc(MAX_STRING_LENGTH * sizeof(char));
+    if(input== NULL){
+        printf("Memory allocation failed");
+        return 1;
+    }
     while (1) {
         currentDirectory();
-        if (fgets(choice, 50, stdin) == NULL) break;
-        choice[strcspn(choice, "\n")] = '\0';
-        if (strlen(choice) == 0)
+        if (fgets(input, MAX_STRING_LENGTH, stdin) == NULL) break;
+        input[strcspn(input, "\n")] = '\0';
+        if (strlen(input) == 0)
             continue;
-        if (strcmp(choice, "exit") == 0)
+        if (strcmp(input, "exit") == 0)
             break;
-        // performOperation(choice);
-        printf("You entered: %s", choice);
+        execute(input);
     }
-    free(choice);
+    free(input);
     free(root);
+    struct FreeBlockNode *temp = blockListHead;
+    while(temp){
+        struct FreeBlockNode *next = temp->next;
+        free(temp);
+        temp = next;
+    }
+
     return 0;
 }
